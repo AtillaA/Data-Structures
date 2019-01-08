0e347d38fcf030739f0c7a1a604190f97e8e384d
Atilla Nalcaci <atillanalcaci@sabanciuniv.edu>
2019-01-08 04:16:00 +0300

    Add files via upload

---
+++ Homework4-Skyline/main.cpp
@@ -0,0 +1,214 @@
+//------------------------------------------//
+//			 Atilla Alpay Nalcaci			//
+//				    19510					//
+//				 19.12.2018					//
+//			  CS300 - Homework 4			//
+//------------------------------------------//
+
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <math.h>
+#include <limits>
+#include <cmath>
+#include <cstdlib>
+#include <algorithm>
+#include <math.h>
+
+#include "mpq.h"	// Include Modified Priority Queue header.
+
+using namespace std;
+
+void lowerCase(string &s);
+
+int main ()
+{
+	/**
+	* Welcome message.
+	*/
+	cout << "-------------------------------------------------------------------------------" << endl;
+	cout << "--########---##----##----##----##---##---------########---##----##---########--" << endl;
+	cout << "--##---------##--##-------##--##----##------------##------###---##---##--------" << endl;
+	cout << "--########---####-----------##------##------------##------##-##-##---########--" << endl;
+	cout << "--------##---##**##---------##------##------------##------##---###---##--------" << endl;
+	cout << "--########---##----##-------##------########---########---##----##---########--" << endl;
+	cout << "-------------------------------------------------------------------------------" << endl;
+
+	cout << "\n------------------------------------------------------------------" << endl;
+	cout << "--To exit the program, enter 'abort', 'exit' or 'x' as file name--" << endl;	
+	cout << "------------------------------------------------------------------\n" << endl;
+
+	bool loop = true;
+
+	while (loop)
+	{
+		/**
+		* Local Variables.
+		*/
+		ifstream inputFile;
+		string fileName, check;
+		int right, left, size, num, tag, height, last, id;
+		int indexval = 1;
+		int k = 0;
+		int l = 0;
+		
+		/**
+		* Open the file with the 'inputName' that is entered.
+		*/
+		cout << "Please enter a file name (with the extension): ";		
+		cin >> fileName;
+
+		check = fileName;
+		lowerCase(check);
+
+		/**
+		* If the entered value is 'abort', exit the program.
+		*/
+		if ((check == "abort") || (check == "exit") || (check == "x"))
+		{
+			loop = false;
+			break;
+		}
+
+		inputFile.open(fileName);
+
+		/**
+		* If the file does not exist, keep asking for a new name until a valid entry occurs.
+		* Check for 'exit' conditions at each step. 
+		*/
+		while (inputFile.is_open() == false)
+		{
+			cout << "Unable to open file. Please try again; ";
+			cin >> fileName;
+
+			check = fileName;
+			lowerCase(check);
+
+			if ((check == "abort") || (check == "exit") || (check == "x"))
+			{
+				loop = false;
+				break;
+			}
+
+			inputFile.open(fileName);
+		}
+
+		/**
+		* If program aborted, break.
+		*/
+		if (loop == false)
+		{
+			break;
+		}
+	
+		inputFile >> id;
+
+		/**
+		* If there are no buildings, terminate. 
+		*/
+		if (id == 0)
+		{
+			inputFile.close();
+			cout << 0 << " " << 0 << endl;
+			return 0;
+		}
+
+		size = ((id * 2) + 1);
+		coord * bps = new coord[size];
+		coord * bpfirst = new coord(0, 0, 0, true);
+
+		bps[0] = *bpfirst;
+
+		/**
+		* Taking the inputs from the file and inserting into the array. 
+		*/
+		for (k = 0; k < id; k++)
+		{
+			/**
+			* Extracting coordinates and height of the skyscraper. 
+			*/
+			tag = indexval;
+			inputFile >> left >> num >> right;
+
+			bpfirst = new coord(right, tag, num, false);
+			bps[indexval + 1] = *bpfirst;
+
+			bpfirst = new coord(left, tag, num, true);
+			bps[indexval] = *bpfirst;
+
+			indexval++;
+			indexval++;
+		}
+
+		/**
+		* Sort the array with respect to start and end coordinates. 
+		*/
+		sort(bps, (bps + size));
+
+		MPQ myheap(size);
+
+		cout << "-----\n";
+
+		/**
+		* Main loop for printing the building values.
+		*/
+		for (l = 0; l < size; l++)
+		{
+			if (bps[l].begin)
+			{
+				height = bps[l].point;
+				last = myheap.maximum();
+				myheap.insertion(height, bps[l].tag);
+
+				/**
+				* Output the building if the maximum value has changed.
+				*/
+				if (last != myheap.maximum()) { cout << bps[l].var << " " << height << "\n"; }
+			}
+
+			else 
+			{
+				last = myheap.maximum();
+				height = bps[l].point;
+				myheap.Remove(bps[l].tag);
+
+				/**
+				* Output the building if the maximum value has changed.
+				*/
+				if (myheap.maximum() != last) {  cout << bps[l].var << " " << myheap.maximum() << "\n"; }
+			}
+		}
+		
+		/**
+		* Close input stream and loop.
+		*/
+		inputFile.close();
+
+		cout << "-----";
+		cout << endl;
+	}
+
+	cout << "\nExiting..." << endl;
+
+	cin.ignore();
+	cin.get();
+	return 0;
+}
+
+/**
+* Function that converts the given string to lowercase.
+*/
+void lowerCase(string &s) 
+{
+	int i = 0;
+
+	for (i = 0; i < s.length(); i++)
+	{
+		if ((s[i] >= 'A') && (s[i] <= 'Z'))
+		{
+			s[i] = (s[i] + 32);
+		}
+	}
+}
---
+++ Homework4-Skyline/mpq.h
@@ -0,0 +1,261 @@
+//------------------------------------------//
+//			 Atilla Alpay Nalcaci			//
+//				    19510					//
+//				 19.12.2018					//
+//			 CS300 - Homework 4				//
+//    Modified Priority Queue Header File   //
+//------------------------------------------//
+
+/**
+* All class definitions and functions provided below are gathered & edited from lecture slides of 2018/19 Fall term.
+*/
+
+#ifndef MPQ_H
+#define MPQ_H
+
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <math.h>
+#include <limits>
+#include <cmath>
+#include <cstdlib>
+#include <algorithm>
+#include <math.h>
+
+using namespace std;
+
+/**
+* Structure for the elements of modified priority queue.
+*/
+struct element
+{
+	int tag;
+	int num;
+
+	element ()
+	{ }
+
+	element ( int num, int tag ) : num( num ), tag( tag )
+	{ }
+};
+
+/**
+* Structure for arrays that stores the coordinates.
+*/
+struct coord
+{
+	int tag;
+	int var;
+	int point;
+
+	bool begin;
+
+	coord( )
+	{ }
+
+	coord( int direction, int tag, int val, bool initiate ) : var( direction ), tag( tag ), point( val ), begin( initiate )
+	{ }
+};
+
+/**
+* Modified Priority Queue Class.
+*/
+class MPQ
+{
+	public:
+		/**
+		* Default constructor.
+		*/
+		MPQ( const int size ) : mpq( new element[size] ), loc( new int[ size ] ), size( size ), curr( 0 )
+		{ }
+
+		/**
+		* Destructor.
+		*/
+		~MPQ ( ) 
+		{ delete [ ] loc; }
+
+		void insertion( int point, int tag );
+		int Remove( int tag );
+		void percolateDown( int val );
+		int maximum( );
+		bool check( ); 
+
+	protected:
+		// None.
+
+	private:
+
+		int * loc;
+		int tmp;
+		int curr;
+
+		element * mpq;
+		const int size;
+};
+
+/**
+* Extracts the maximum value from the function.
+* Returns the maximum value if there exists any.
+* Returns 0 otherwise.
+*/
+int MPQ::maximum( )
+{
+	if ( !check( ) )
+	{ 
+		return mpq[ 1 ].num;
+	}
+
+	else 
+	{ 
+		return -1;
+	}
+}
+
+/**
+* Checks if the heap is empty.
+* Returns the initialization of current size to zero.
+*/
+bool MPQ::check( ) 
+{ 
+	return ( curr == 0 );
+}
+
+/**
+* Function that removes the element with tag and returns its value.
+* Returns zero if no such element exists or if the heap is empty.
+* Otherwise, it swaps the element with tag with the last element of the heap.
+* Updates the location of descendant.
+*/
+int MPQ::Remove( int tag )
+{
+	int tagloc = loc[ tag ];
+	int tmp = mpq[ tagloc ].num;
+
+	if ( check( ) || !( loc[ tag ] ) )
+	{
+		return 0;
+	}
+
+	mpq[ tagloc ] = mpq[ curr-- ];
+	loc[ mpq[ curr + 1 ].tag ] = tagloc;
+    percolateDown( tagloc );
+	tmp = mpq[ 1 ].num;							// 'tmp' stores the maximum value.
+
+	return tmp;
+}
+
+/**
+* Function that perlocates down the modified heap.
+* In main loop, it perlocates the hole by comparing left and right descendants.
+* After comparison, it either perlocates (if necessary) or skips.
+*/
+void MPQ::percolateDown( int val )
+{
+	int descendant;
+	int tmp = mpq[ val ].num;
+	int tmptag = mpq[ val ].tag;
+
+	do
+	{
+		descendant = ( val * 2 );
+
+		if( ( descendant != curr ) && ( mpq[ descendant + 1 ].num > mpq[ descendant ].num ) )
+		{ 
+			descendant++;
+		}	
+			
+		else if( mpq[ descendant ].num > tmp )
+		{ 
+			mpq[ val ] = mpq[ descendant ];
+			loc[ mpq[ descendant ].tag ] = val;
+		}
+
+		else
+		{ 
+			break;
+		}
+
+		val = descendant;
+	} while( ( val * 2 ) <= curr );
+
+	mpq[ val ].tag =  tmptag;					// After each operation, update the values accordingly.
+	loc[ tmptag ] = val;
+	mpq[ val ].num = tmp;
+}
+
+/**
+* Function that inserts the current element into the modified priority queue.
+* Creates a hole value and starts perlocation, updates the location variable after each perlocate.
+* After execution, sets the hole values appropriately and updates the location.
+*/
+void MPQ::insertion( int t, int tag )
+{
+	curr++;
+
+	int newval = curr;							// Integer that holds the current hole value.
+
+	while ( ( t > mpq[ newval / 2 ].num) && ( newval > 1 ) )
+	{ 
+		mpq[ newval ] = mpq[ newval / 2 ];
+		loc[ mpq[ newval / 2 ].tag ] = newval;
+		newval /= 2;
+	}
+
+	mpq[ newval ].num = t;						// After each operation, update the values accordingly.
+	mpq[ newval ].tag = tag;
+	loc[ tag ] = newval;
+
+}
+
+/**
+* Operator function for comparing the start/end points of the skyscrapers.
+*/
+bool operator<( const coord & left, const coord & right ) 
+{
+	if ( left.var < right.var )
+	{ 
+		return true;
+	}
+
+	else if ( left.var == right.var )
+	{	
+		/**
+		* Condition for putting the taller building before the shorter one (for comparing their start points).
+		*/
+		if ( ( right.begin ) && ( left.begin ) )
+		{ 
+			return ( right.point < left.point );
+		}
+
+		/**
+		* Condition for putting the shorter building before the taller one (for comparing their end points).
+		*/
+		else if ( !( right.begin ) && !( left.begin ) )
+		{ 
+			return ( right.point > left.point );
+		}
+		
+		/**
+		* Condition for comparing the start and end points of two buildings.
+		* Places the building with start point before the one with the end point.
+		*/
+		else
+		{
+			return ( left.begin );
+		}
+	}
+
+	/**
+	* Return false if the left hand side is taller than the right.
+	*/
+	else
+	{
+		return false;
+	}
+}
+
+#endif
